57f81aac29b80ff940ec036a0034b22e642c5dd2
Daniel <leedanie2@grinnell.edu>
2026-04-06 21:05:38 -0500

    Reshuffled files

---
+++ src/main/java/edu/ttap/maps/SubstitutionCipher.java
@@ -0,0 +1,109 @@
+package edu.ttap.maps;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Scanner;
+
+import edu.ttap.maps.AssociationList.Pair;
+/**
+ * A substitution cipher is a simple encryption scheme that associates each
+ * letter of the alphabet with a different letter.
+ */
+public class SubstitutionCipher {
+    /**
+     * Creates a substitution cipher by reading a mapping of characters from the given
+     * file. Each mapping of the file should be of the form "a b", where 'a' is mapped to
+     * 'b' in the cipher. We require 
+     * @param filename the name of the file containing the mapping
+     * @return the cipher as a mapping between characters
+     */
+    public static Map<Character, Character> createCipher(String filename) throws IOException {
+        ArrayList<Pair> lst = null;
+        AssociationList<Character, Character> map = new AssociationList<>(lst);
+        Scanner scan = new Scanner(new File(filename));
+        while(scan.hasNextLine()) {
+            char key = scan.next().charAt(0);
+            char value = scan.next().charAt(0);
+            map.put(key, value);
+        }
+        return map;
+    }
+
+    /**
+     * Determines whether the given mapping is a valid substitution cipher. A cipher is
+     * valid if (a) it maps every letter of the alphabet (a–z) and (b) it is a bijection,
+     * i.e., no two letters map to the same letter (so that we can roundtrip encode/decode
+     * a message without loss of fidelity).
+     * @param cipher
+     * @return true iff the given mapping is a valid substitution cipher
+     */
+    public static boolean isValidCipher(Map<Character, Character> cipher) {
+        for(char c = 'a'; c <= 'z'; c ++) {
+            if(!cipher.containsKey(c) && !cipher.containsValue(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Given a valid substitution cipher, produces the inverse mapping, which
+     * can be used to decode the encoded massage. For example, if the cipher
+     * maps 'a' to 'b', then the inverse mapping should map 'b' to 'a'.
+     * @param cipher the cipher to invert
+     * @return the inverse mapping of the given cipher
+     */
+    public static Map<Character, Character> invertCipher(Map<Character, Character> cipher) {
+        ArrayList<Pair> lst = null;
+        Map<Character, Character> inverse = new AssociationList<>(lst);
+        for(Map.Entry<Character, Character> entry : cipher.entrySet()) {
+            inverse.put(entry.getValue(), entry.getKey());
+        }
+        return inverse;
+    }
+
+    /**
+     * Translates the given string using the provided mapping.
+     * @param s the string to translate
+     * @param mapping the mapping to use
+     * @return the translated string
+     */
+    public static String translate(String s, Map<Character, Character> mapping) {
+        String mapped = null;
+        for(int i = 0; i < s.length(); i++) {
+            mapped = mapped + mapping.get(s.charAt(i));
+        }
+        return mapped;
+    }
+
+
+    /**
+     * The main driver for the substitution cipher program.
+     * @param args the driver's command-line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        if (args.length != 3) {
+            System.err.println(
+                "Usage: java SubstitutionCipher <encode|decode> <cipherfile> <filename>");
+            System.exit(1);
+        }
+        String mode = args[0];
+        String path = args[1];
+        String text = args[2];
+
+        String output = "null";
+        Map<Character, Character> map = createCipher(path);
+
+        if (mode.equals("encrypt")) {
+            output = translate(text, map);
+        } else if (mode.equals("decrypt")) { 
+            map = invertCipher(map);
+            output = translate(text, map);
+        } else {
+            throw new IOException("First argument must be encrypt or decrypt!");
+        }
+        System.out.println(output);
+    }
+}
